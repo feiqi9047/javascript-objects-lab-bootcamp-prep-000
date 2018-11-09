799efb53a5c42741ee9cc2e7b59dfc3304332d01
Fei Qi <feiqi9047@gmail.com>
2018-11-09 22:13:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key){
-  delete object.]
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.[key]
+  return object
 }
